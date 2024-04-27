810e52eefb7ace5977c3cf4d04041afcdbc15e49
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-04-27 19:08:30 +0700

    remove unused

--- src/app/(dashboard)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ src/app/(dashboard)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -103,7 +103,6 @@ const ChaptersList = ({ items, onReorder, onEdit, ...props }: ChaptersListProps)
           </div>
         )}
       </Droppable>
-      ChaptersList
     </DragDropContext>
   );
 }
