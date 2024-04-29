5cc61e60264977ac0257344f9ff44fbb0bb37760
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-04-29 07:53:49 +0700

    Teacher Analytics

---
+++ src/app/(dashboard)/teacher/analytics/_components/chart.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { Card } from "@/_components/ui/card";
+import {
+  Bar,
+  BarChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+type ChartProps = {
+  data: { name: string, total: number }[];
+}
+
+export const Chart = ({ data }: ChartProps) => {
+  return (
+    <Card className="p-4">
+      <ResponsiveContainer width="100%" height={350}>
+        <BarChart data={data}>
+          <XAxis
+            dataKey="name"
+            stroke="#888888"
+            fontSize={12}
+            tickLine={false}
+            axisLine={false}
+          />
+          <YAxis
+            dataKey="total"
+            stroke="#888888"
+            fontSize={12}
+            tickLine={false}
+            axisLine={false}
+            tickFormatter={(value) => `$${value}`}
+          />
+          <Bar
+            dataKey="total"
+            fill="#0369a1"
+            radius={[4, 4, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+}
--- src/app/(dashboard)/teacher/analytics/page.tsx
+++ src/app/(dashboard)/teacher/analytics/page.tsx
@@ -1,8 +1,49 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/_components/ui/card";
+import { getAnalytics } from "@/actions/get-analytics";
+import { formatPrice } from "@/lib/format";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { Chart } from "./_components/chart";
+
 type Props = {}
 
-const AnalyticsPage = (props: Props) => {
+const AnalyticsPage = async (props: Props) => {
+  const { userId } = auth();
+  if (!userId) return redirect("/");
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
   return (
-    <div>AnalyticsPage</div>
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+      <Chart data={data}/>
+    </div>
+  );
+}
+
+type DataCardProps = {
+  value: number;
+  label: string;
+  shouldFormat?: boolean;
+}
+
+const DataCard = ({ value, label, shouldFormat }: DataCardProps) => {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">
+          {label}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">
+          {shouldFormat ? formatPrice(value) : value}
+        </div>
+      </CardContent>
+    </Card>
   );
 }
 
